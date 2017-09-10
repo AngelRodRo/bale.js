dbdb5b8d24bc5b61dc4523691149884079553c0c
Angel <aleangrodriguez123@gmail.com>
2017-09-10 10:56:11 -0500

    Adding postgres driver

--- dbw.js
+++ dbw.js
@@ -1,5 +1,4 @@
 
-
 module.exports = function(config){
 
     let driver = config.driver;
@@ -11,18 +10,38 @@ module.exports = function(config){
         return this;    
     }
 
+    const parseData = (data) => {
+        const lengthKeys = Object.keys(data).length;
+        const dataKeys = Object.keys(data).join(",");
+        const values = Object.values(data);
+
+        let dataValues = "";
+        let i;
+
+        for (i = 1; i <= lengthKeys-1; i++) {
+            dataValues += `${i},`
+        }
+        dataValues += `${i+1}`;
+
+        return [dataKeys, dataValues, values];
+    }
+
     this.insert = function(data){
         switch(driver){
-            case 'mongodb':
+            case "mongodb":
                 return this.db.collection(resourceName).insert(data);
                 break;
+            case "postgres":
+                const [dataKeys, dataValues, values] = parseData(data);
+                const text = `INSERT INTO ${resourceName}(${dataKeys}) VALUES (${dataValues})`;
+                return db.query(text, values);
         }
     }
 
-    let mongo = ()=>{
-        let mongodb = require('mongodb');
-        let MongoClient = mongodb.MongoClient;
-        let url = "mongodb://localhost:27017/"+config.dbname;
+    const mongo = () => {
+        const mongodb = require("mongodb");
+        const MongoClient = mongodb.MongoClient;
+        const url = `mongodb://${config.host}:${config.port}/${config.dbname}`;
         
         return function(){
             return new Promise((resolve,reject)=>{
@@ -32,19 +51,43 @@ module.exports = function(config){
                 });
             })            
             .then((db)=>{
-                
                 this.db = db;
                 db.dropDatabase();
                 return this;
             }).catch(e=>console.log(e));
         }
     }
-   
 
+    const postgres = () => {
+        const { Client } = require("pg");
+        const client = new Client();
+
+        const client = new Client({
+            user: config.user,
+            host: config.host,
+            database: config.dbname,
+            password: config.password,
+            port: config.port,
+        });
+
+        return function() {
+            return new Promise((resolve, reject) => {
+                client.connect();
+                resolve(client);
+            })
+            .then((client) => {
+                this.db = client;
+                return this;
+            }).catch(e => console.log(e));
+        }
+    }
+   
     switch(driver){
-        case 'mongodb':
+        case "mongodb":
             this.connect = mongo();
             break;
+        case "postgres":
+            
     }
 
 };
